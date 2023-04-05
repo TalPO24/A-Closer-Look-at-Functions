05642edef97facc5fce279e335a4aacfa97a3f46
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-05 14:46:21 +0300

    A Closer Look at Functions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 //* Default Parameters
 const bookings = []
 
@@ -22,3 +23,36 @@ createBooking('LH123', 2, 800)
 createBooking('LH123', 2)
 createBooking('LH123', 5)
 createBooking('LH123', undefined, 1000) // We use undefined to set the 'numPassengers' to the default value.
+*/
+
+//* How passing arguments works: Value vs. Reference
+const flight = 'LH234'
+const jonas = {
+    name: 'Jonas Schmedmann',
+    passport: 23748567
+}
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'LH999'
+    passenger.name = 'Mr.' + passenger.name
+
+    if (passenger.passport === 23748567) {
+        alert('Check in')
+    } else {
+        alert('Wrong passport!')
+    }
+}
+
+// checkIn(flight, jonas)
+// console.log(flight)
+// console.log(jonas)
+
+// const flightNum = flight
+// const passenger = jonas
+
+const newPassport = function(person) {
+    person.passport = Math.trunc(Math.random() * 100000000000)
+}
+
+newPassport(jonas)
+checkIn(flight, jonas)
