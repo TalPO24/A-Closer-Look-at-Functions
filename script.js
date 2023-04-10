c290b4396d19d716b0d3c77cf72e3603aed8d290
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-10 13:28:56 +0300

    A-Closer-Look-at-Functions

--- script.js
+++ script.js
@@ -264,7 +264,7 @@ console.log(notPrivate)
 */
 
 //* Closures
-
+// Closure in JavaScript is a form of scoping used to preserve variables from the outer scope of a function in the inner scope of a function.
 const secureBooking = function() {
     let passengerCount = 0
 
@@ -281,3 +281,43 @@ booker()
 booker()
 
 console.dir(booker)
+
+//* More Closures Examples
+// Example 1
+let f;
+
+const g = function() {
+    const a = 23
+    f = function() {
+        console.log(a * 2)
+    }
+}
+const h = function() {
+    const b = 777
+    f = function() {
+        console.log(b * 2)
+    }
+}
+
+g()
+f()
+console.dir(f)
+    // Re-assigning f function
+h()
+f()
+console.dir(f)
+
+// Example 2
+
+const boardPassengers = function(n, wait) {
+    const perGroup = n / 3
+
+    setTimeout(function() { // this is a call back function 
+        console.log(`We are now boarding all ${n} passengers`)
+        console.log(`There are 3 groups, each with ${perGroup}`)
+    }, wait * 1000)
+
+    console.log(`Will start boarding in ${wait} seconds`)
+}
+
+boardPassengers(180, 3)
